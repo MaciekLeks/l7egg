8e4fb12dbacd00a28548606523f25c0e9848cccc
Maciej Leks <maciej.leks@gmail.com>
2023-04-27 16:49:05 +0200

    Makefile target k8s-build-crds modified by removing tools/tools.go - no needed since golang 1.17

--- tools/generate.go
+++ tools/generate.go
@@ -1,3 +1,3 @@
 package tools
 
-//go:generate go run sigs.k8s.io/controller-tools/cmd/controller-gen crd paths=../pkg/apis/... output:dir=../crds
+//go:generate go run sigs.k8s.io/controller-tools/cmd/controller-gen@latest crd paths=../pkg/apis/... output:dir=../crds
--- tools/tools.go
+++
@@ -1,8 +0,0 @@
-//go:build tools
-
-package tools
-
-// Force direct dependency on code-generator so that it may be executed with go run
-import (
-	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"
-)
