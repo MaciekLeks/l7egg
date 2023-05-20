b776e4921c177b8f45ece3a8c1f03c35ba24433d
Maciej Leks <maciej.leks@gmail.com>
2023-05-20 13:30:50 +0200

    SafeMap added #2

--- pkg/tools/safe_slice.go
+++ pkg/tools/safe_slice.go
@@ -6,14 +6,14 @@ import (
 )
 
 type SafeSlice[T any] struct {
-	mux   sync.RWMutex
+	sync.RWMutex
 	slice []T
 }
 
 // Append is a thread-safe version of the Go built-in append function.
 func (ss *SafeSlice[T]) Append(item ...T) {
-	ss.mux.Lock()
-	defer ss.mux.Unlock()
+	ss.Lock()
+	defer ss.Unlock()
 
 	ss.slice = append(ss.slice, item...)
 
@@ -22,33 +22,32 @@ func (ss *SafeSlice[T]) Append(item ...T) {
 
 // Len is a thread-safe function to get the length of the inner slice.
 func (ss *SafeSlice[T]) Len() int {
-	ss.mux.RLock()
-	defer ss.mux.RUnlock()
+	ss.RLock()
+	defer ss.RUnlock()
 
 	return len(ss.slice)
 }
 
 // Set is a thread-safe function to assign a new value to a key in the inner slice.
 func (ss *SafeSlice[T]) Set(key int, value T) {
-	ss.mux.Lock()
-	defer ss.mux.Unlock()
+	ss.Lock()
+	defer ss.Unlock()
 
 	ss.slice[key] = value
 }
 
 // Update is a thread-safe function to set value's fields for a key in the inner slice.
 func (ss *SafeSlice[T]) Update(key int, updateFunc func(val *T)) {
-	ss.mux.Lock()
-	defer ss.mux.Unlock()
+	ss.Lock()
+	defer ss.Unlock()
 
 	updateFunc(&ss.slice[key])
 }
 
 // Get is a thread-safe function to get a value by key in the inner slice.
 func (ss *SafeSlice[T]) Get(key int) T {
-
-	ss.mux.RLock()
-	defer ss.mux.RUnlock()
+	ss.RLock()
+	defer ss.RUnlock()
 
 	return ss.slice[key]
 }
