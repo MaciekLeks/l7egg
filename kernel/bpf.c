ac51d1ada1f9bfac2264031acdf180483c9eaa39
Maciej Leks <maciej.leks@gmail.com>
2024-01-08 10:02:15 +0100

    Fix conditional directive in kernel/bpf.c for debug mode
    Corrected the format of an #if preprocessor directive for DEBUG mode in the kernel/bpf.c file. This modification ensures the debug print functions are only compiled when DEBUG is defined and equals 1, contributing to more efficient code execution in non-debug environments.

--- kernel/bpf.c
+++ kernel/bpf.c
@@ -210,7 +210,7 @@ static __always_inline long ipv6_update(__u8 ipaddr[16], struct value_t val, __u
     return bpf_map_update_elem(&ipv6_lpm_map, &key, &val, BPF_EXIST);
 }
 
-#if not defined DEBUG || DEBUG == 1
+#ifndef DEBUG || DEBUG == 1
 static __always_inline void ipv4_print_ip(char *prefix, char *suffix, __u32 ip) {
     unsigned char bytes[4];
     bytes[0] = ip & 0xFF;
